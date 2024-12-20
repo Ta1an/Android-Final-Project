563e87b93fae4aed91d1b2ff67ef4847fcd79d78
Ta1an <alentoleubek@gmail.com>
2024-12-21 01:37:42 +0500

    Changes in DB

--- app/src/main/java/com/example/thenewsapp/database/ArticleDB.kt
+++ app/src/main/java/com/example/thenewsapp/database/ArticleDB.kt
@@ -1,6 +1,8 @@
 package com.example.thenewsapp.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.thenewsapp.models.Article
@@ -19,5 +21,18 @@ abstract class ArticleDB : RoomDatabase() {
         @Volatile
         private var instance : ArticleDB? = null
         private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: createDatabase(context).also{
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDB::class.java,
+                "article_db.db"
+            ).build()
     }
 }
