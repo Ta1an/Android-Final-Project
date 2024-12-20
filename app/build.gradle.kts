cc78f7ca56534d840ef279387f8a7788ccff2b8f
Ta1an <alentoleubek@gmail.com>
2024-12-20 20:13:26 +0500

    Restarting porject

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.example.newsprojectpractice"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.newsprojectpractice"
+        minSdk = 28
+        //noinspection OldTargetApi
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx.v1120)
+    implementation(libs.androidx.appcompat.v161)
+    implementation(libs.material.v1100)
+    implementation(libs.androidx.constraintlayout.v214)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
+
+    // Architectural Components
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Room
+    implementation (libs.androidx.room.runtime.v260)
+    ksp (libs.androidx.room.compiler.v260)
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation (libs.androidx.room.ktx.v260)
+
+    // Coroutines
+    implementation (libs.kotlinx.coroutines.core.v171)
+    implementation (libs.kotlinx.coroutines.android.v171)
+
+    // Coroutine Lifecycle Scopes
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.runtime.ktx.v287)
+
+    // Retrofit
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.logging.interceptor)
+
+    // Navigation Components
+    implementation (libs.androidx.navigation.fragment.ktx.v285)
+    implementation (libs.androidx.navigation.ui.ktx.v285)
+
+    // Glide
+    implementation (libs.glide.v4120)
+    ksp (libs.compiler)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val navVersion = "2.7.5"
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+}
